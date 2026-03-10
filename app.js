605c8cd95dd2410872f5f7e421d156913e717c0b
sanjayv23 <sanjayvelu2311@gmail.com>
2026-03-10 12:22:47 +0530

    init EasyFin project

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const port=3000;
 app.get("/",(req,res)=>{
   res.send("hi");  
 })
-
+app.get("/liabilities",(req,res)=>{
+    
+});
 app.listen(port, (req,res)=>{
     console.log(`Listening from port ${port}`)
 })

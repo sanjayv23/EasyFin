e6329e50765ad555875be2c65dc2ca319853c80d
sanjayv23 <sanjayvelu2311@gmail.com>
2026-03-24 22:53:36 +0530

    feat: asset tracking feat added

--- Backend/app.js
+++ Backend/app.js
@@ -12,14 +12,14 @@ const db = new pg.Client({
   password: "232004",
   port: 5432,
 });
-const JWT_SECRET="sanjaymajasooo"
+const JWT_SECRET = "sanjaymajasooo";
 db.connect();
 app.use(express.json());
-app.use(body.urlencoded({extended:true}));
+app.use(body.urlencoded({ extended: true }));
 
 const authMiddleware = (req, res, next) => {
   const userId = req.header("x-user-id");
-  console.log(req.header("authorization"))
+  console.log(req.header("authorization"));
   if (!userId) {
     return res.status(401).json({
       message: "Unauthorized: missing user id",
@@ -31,15 +31,15 @@ const authMiddleware = (req, res, next) => {
 };
 
 const JWTauthMiddleware = (req, res, next) => {
-  const authheader =req.header("authorization").split(' ')[1];
-  console.log(authheader)
+  const authheader = req.header("authorization").split(" ")[1];
+
   if (!authheader) {
     return res.status(401).json({
       message: "UNo token, authorization denied",
     });
   }
-  console.log(" verift "+jwt.verify(authheader,JWT_SECRET))
-  req.user = jwt.verify(authheader,JWT_SECRET);
+  console.log(" verift " + JSON.stringify(jwt.verify(authheader, JWT_SECRET)));
+  req.user = jwt.verify(authheader, JWT_SECRET);
   next();
 };
 
@@ -47,31 +47,62 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-//USERS- 
-app.post("/api/user/login",async (req,res)=>{
-  const userEmail=req.body.email;
-  const token=jwt.sign(
-    {id:userEmail},
-    JWT_SECRET,
-    {expiresIn:'1h'}
-  )
-  res.json({token});
-})
-// LOANS - 
+//USERS-
+app.post("/api/user/login", async (req, res) => {
+  const {mail,password} = req.body;
+  
+  
+  if(!mail || !password){
+    return res.status(400).json({
+      message: "Email and password are required"
+    })
+  }
+  
+  try {
+    const query = `select user_id from users where mail=$1 and password=$2`;
+    const values=[ mail,password]; 
+    const result = await db.query(query, values);
+    if(result.rowCount==0){
+      return res.status(404).json({
+      message: "user not found"
+    }) 
+    }
+    console.log("Result "+result.rows[0]);
+    const user_id = result.rows[0].user_id;
+    const token = jwt.sign(
+      { id: user_id, mail: mail },
+
+      JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+    return res.status(200).json({ 
+      token,
+      
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Invalid credentials",
+      error: error.message,
+    });
+  }
+  
+});
+
+
+// LOANS -
 
 app.post("/api/loans", authMiddleware, async (req, res) => {
   const userId = req.user.id;
-  console.log("user ID "  + userId);
-  console.log("req  "  + JSON.stringify(req.body));
+  console.log("user ID " + userId);
+  console.log("req  " + JSON.stringify(req.body));
   const {
     id,
     loanName,
     loanType,
-    
+
     outstandingAmount,
     interestRate,
     monthlyEmi,
-    
   } = req.body;
 
   const query = `
@@ -93,16 +124,16 @@ app.post("/api/loans", authMiddleware, async (req, res) => {
     id,
     loanName,
     loanType,
-    
+
     outstandingAmount,
     interestRate,
     monthlyEmi,
-    userId
+    userId,
   ];
 
   try {
     const result = await db.query(query, values);
-    
+
     res.status(201).json({
       message: "Loan created successfully",
       loan: result.rows[0],
@@ -141,8 +172,8 @@ app.get("/api/loans", authMiddleware, async (req, res) => {
 
 app.get("/api/loans/:id", authMiddleware, async (req, res) => {
   const user_id = req.user.id;
-  const id=req.params.id;
-  console.log(user_id+" "+id);
+  const id = req.params.id;
+  console.log(user_id + " " + id);
   const query = `
     SELECT *
     FROM loans
@@ -151,7 +182,7 @@ app.get("/api/loans/:id", authMiddleware, async (req, res) => {
   `;
 
   try {
-    const result = await db.query(query, [user_id,id]);
+    const result = await db.query(query, [user_id, id]);
     res.status(200).json({
       message: "Loans fetched successfully",
       loans: result.rows,
@@ -165,9 +196,8 @@ app.get("/api/loans/:id", authMiddleware, async (req, res) => {
 });
 
 app.get("/api/loans/amount/:userId", authMiddleware, async (req, res) => {
-  
-  const userId=req.params.userId;
-  
+  const userId = req.params.userId;
+
   const query = `
     SELECT sum(outstanding_amount)
     FROM loans
@@ -178,7 +208,7 @@ app.get("/api/loans/amount/:userId", authMiddleware, async (req, res) => {
 
   try {
     const result = await db.query(query, [userId]);
-    console.log(result.rows[0])
+    console.log(result.rows[0]);
     res.status(200).json({
       message: "Loans fetched successfully",
       loans: result.rows,
@@ -190,25 +220,31 @@ app.get("/api/loans/amount/:userId", authMiddleware, async (req, res) => {
     });
   }
 });
-app.put("/api/loans/:id",authMiddleware,async (req,res)=>{
-  
-  const userId=req.user.id;
-  const loanId=req.params.id;
-  
+app.patch("/api/loans/:id", authMiddleware, async (req, res) => {
+  const userId = req.user.id;
+  const loanId = req.params.id;
+
   const {
-    loan_name, 
-    loan_type, 
-    outstanding_amount, 
-    interest_rate, 
-    monthly_emi
-  }=req.body;
-  console.log(loan_name+" "+ 
-    loan_type+" "+
-    outstanding_amount+" "+ 
-    interest_rate+" "+
-    monthly_emi+" ")
+    loan_name,
+    loan_type,
+    outstanding_amount,
+    interest_rate,
+    monthly_emi,
+  } = req.body;
+  console.log(
+    loan_name +
+      " " +
+      loan_type +
+      " " +
+      outstanding_amount +
+      " " +
+      interest_rate +
+      " " +
+      monthly_emi +
+      " ",
+  );
   // coalesce ($1,loan_name) takes $1 or take already
-  const query=`
+  const query = `
     update loans
     set loan_name=Coalesce($1,loan_name),
     loan_type=Coalesce($2,loan_type),
@@ -219,30 +255,29 @@ app.put("/api/loans/:id",authMiddleware,async (req,res)=>{
     returning *
   `;
 
-  console.log(monthly_emi || null)
-  const values=[
-    loan_name || null, 
-    loan_type || null, 
-    outstanding_amount || null, 
-    interest_rate || null, 
+  console.log(monthly_emi || null);
+  const values = [
+    loan_name || null,
+    loan_type || null,
+    outstanding_amount || null,
+    interest_rate || null,
     monthly_emi || null,
     userId,
-    loanId
-  ]
+    loanId,
+  ];
   try {
-    const result=await db.query(query,values);
+    const result = await db.query(query, values);
     res.status(200).json({
-      message:"Update Success",
-      updatedLoan:result.rows
+      message: "Update Success",
+      updatedLoan: result.rows,
     });
-    
   } catch (error) {
     res.status(500).json({
-      message:"Update failed",
-      error:error.message
+      message: "Update failed",
+      error: error.message,
     });
   }
-})
+});
 app.delete("/api/loans/:loan_id", authMiddleware, async (req, res) => {
   const userId = req.user.id;
   console.log(req.params);
@@ -275,16 +310,199 @@ app.delete("/api/loans/:loan_id", authMiddleware, async (req, res) => {
   }
 });
 
-// ASSEST - 
-app.get("/api/asset",JWTauthMiddleware,async(req,res)=>{
-  const userEmail=req.id;
-  console.log("df"+userEmail);
+
+
+// ASSEST - JWT AUTH
+app.get("/api/asset", JWTauthMiddleware, async (req, res) => {
   
-  res.send("df");
+  try{
+    const query=`select * from assets where user_id=$1`;
+    
+    const result=await db.query(query,[req.user.id]);
+    
+    return res.status(200).json({
+      assets:result.rows,
+      
+    })
+  }
+  catch(error){
+    console.error("error "+error.message);    
+    return res.status(500).json({
+      message:"Internal server error",
+      
+    })
+  }
+
+  
+
+  
+});
+
+app.post("/api/asset", JWTauthMiddleware, async (req, res) => {
+  const {
+    current_value,units,invested_value,asset_class,asset_name,isin_code
+  } = req.body;
+    if (!current_value || !units || !invested_value || !asset_class || !asset_name) {
+  return res.status(400).json({ message: "All required fields must be provided" });
+}
+  try{
+    
+    
+    const query=`insert into assets(user_id,current_value,units,invested_value,asset_class,asset_name,isin_code) values($1,$2,$3,$4,$5,$6,$7) returning *`;
+    
+    const values=[
+      req.user.id,current_value,units,invested_value,asset_class,asset_name,isin_code
+    ]
+    const result=await db.query(query,values);
+    
+    
+    return res.status(201).json({
+      message:"Assets Added Sucessfully",
+      assets:result.rows,
+    
+    })
+  }
+  catch(error){
+    console.log("error "+error.message);  
+    return res.status(500).json({
+      message:"Internal server error",
+      
+    })
+  }
+
+  
+
+  
+});
+
+app.get("/api/value/asset", JWTauthMiddleware, async (req, res) => {
+  
+  try{
+    const query=`SELECT SUM(invested_value) AS total_invested, 
+                      SUM(current_value) AS total_current 
+               FROM assets WHERE user_id=$1`;
+    
+    const result=await db.query(query,[req.user.id]);
+    
+    return res.status(200).json({
+      invested_value:result.rows[0].total_invested || 0,
+      current_value:result.rows[0].total_current || 0
+    })
+  }
+  catch(error){
+    console.error("error "+error.message);    
+    return res.status(500).json({
+      message:"Internal server error",
+      
+    })
+  }
+
+  
+
+  
+});
+
+app.delete("/api/asset/:asset_id",JWTauthMiddleware,async (req,res)=>{
+    if(!req.params.asset_id){
+      return res.status(400).json({
+        message:"asset_id is required"
+      })
+    }
+    try{
+    const query=`delete from assets where user_id=$1 and asset_id=$2 returning *`;
+    
+    const result=await db.query(query,[req.user.id,req.params.asset_id]);
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "Asset not found" });
+    }
+    return res.status(200).json({
+      assets:result.rows,
+    
+    })
+  }
+  catch(error){
+    console.error("error "+error.message);    
+    return res.status(500).json({
+      message:"Internal server error",
+      
+    })
+  }
 })
 
+app.patch("/api/asset/:asset_id", JWTauthMiddleware, async (req, res) => {
+  const {
+    current_value,units,invested_value,asset_class,asset_name,isin_code
+  } = req.body;
+    
+  try{
+    
+    
+    const query=`update assets
+    set asset_name=Coalesce($1,asset_name),
+    asset_class=Coalesce($2,asset_class),
+    invested_value=Coalesce($3,invested_value), 
+    current_value=Coalesce($4,current_value), 
+    units=Coalesce($5,units),
+    isin_code=coalesce($6,isin_code)
+    where user_id=$7 and asset_id=$8
+    returning *`;
+    
+    const values=[
+      asset_name || null,asset_class || null,invested_value || null,current_value|| null ,units || null,isin_code || null,req.user.id,req.params.asset_id 
+    ]
+    const result=await db.query(query,values);
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "Asset not found" });
+    }
+    
+    return res.status(200).json({
+      message:"Assets updated Sucessfully",
+      assets:result.rows,
+    
+    })
+  }
+  catch(error){
+    console.log("error "+error.message);  
+    return res.status(500).json({
+      message:"Internal server error",
+      
+    })
+  }  
+  
+});
+
+//Expense and income tracker 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SERVER LISTENING TO PORT
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
-
